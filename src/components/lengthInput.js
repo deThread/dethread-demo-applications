0b80a4c80b604a04fab3e772b6fee795df1ba11e
Dlaosb <Dlaosb@gmail.com>
2016-10-07 15:05:35 -0700

    Fixing Commits

---
+++ src/components/lengthInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LengthInput extends Component{
+render(){
+  return (
+    <div>
+      <input type="number" name="length" placeholder="length" id="length" value={this.props.len} min='0' onChange={this.props.update} />
+    </div>
+  )
+}
+}
---
+++ src/components/perfInputField.js
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+
+// class inputField extends Component{
+// constructor(props){
+//   super(props);
+//   this.state = {length: null, hash : null, workers : null}
+// }
+// render(){
+
+// }
+// }
+
+// export default inputField;
