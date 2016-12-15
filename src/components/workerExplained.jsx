ce9d3df7940892bd836d4b02f7452764217f0f33
Bryan Yee <yee.bryan.c@gmail.com>
2016-12-14 18:42:56 -0800

    Add info button and description for worker input

---
+++ src/components/workerExplained.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const WorkerExplained = () => {
+  return (
+    <div>
+      <div id="worker-info" className="popup instruction-box">
+        <p className="instruction-title">What are web workers?</p>
+        <p>Web workers are a technology that allow a web browser to run multiple
+        simultaneous scripts without interfering with the web page. By allowing the
+        running of multiple web workers, your web browser will be able to process
+        multiple tasks at the same time, at expense of CPU resources.</p>
+      </div>
+    </div>
+  );
+}
+
+export default WorkerExplained;
--- src/components/workerInput.jsx
+++ src/components/workerInput.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-const WorkerInput = function(props) {
-  return (
-    <div>
-      <input className="inputMaterial numInput" type="number" id="workers" placeholder="Number of Workers" name="workers" min="1" max={`${props.optimalWorkers}`} onChange={props.updateSettings} />
-      <span className="highlight"></span>
-      {/* <span className="bar"></span>*/}
-    </div>
-  )
-};
+class WorkerInput extends Component {
+  render() {
+    return (
+      <div>
+        <input className="inputMaterial numInput" type="number" id="workers" placeholder="Number of Workers" name="workers" min="1" max={`${this.props.optimalWorkers}`} onChange={this.props.updateSettings} />
+        <i className="material-icons help-button" onClick={this.props.togglePopup.bind(null, '#worker-info')}>help</i>
+        <span className="highlight"></span>
+        {/* <span className="bar"></span>*/}
+      </div>
+    );
+  }
+}
 
 export default WorkerInput;
